d0a45b12d11d6e5fa93154bc885a2ffb8a248158
sunquan <1585437938@qq.com>
2021-07-14 14:32:40 +0800

    1.新增双向绑定实现原理；2.修改node代码

--- app.js
+++ app.js
@@ -34,5 +34,5 @@ let server = http.createServer((req, res) => {
 })
 
 server.listen(3000, '127.0.0.1', () => {
-    console.log('服务器已经运行，请打开浏览器，输入：http：//127.0.0.1：3000/来访问')
+    console.log('服务器已经运行，请打开浏览器，输入：http://127.0.0.1:3000/test1.html来访问')
 })
